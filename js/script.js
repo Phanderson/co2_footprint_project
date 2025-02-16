4744facfbedb3c2206b774e84a1e1b104393088e
Phanderson <mustafa.tache@outlook.com>
2025-02-16 14:21:40 +0100

    corrects reference link

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function setLanguage(lang) {
+    document.documentElement.setAttribute("dir", lang === "ar" ? "rtl" : "ltr");
+    localStorage.setItem("language", lang);
+  }
+  
+  // Sprache beim Laden setzen
+  window.onload = () => {
+    const savedLang = localStorage.getItem("language") || "de";
+    setLanguage(savedLang);
+  };
+  
